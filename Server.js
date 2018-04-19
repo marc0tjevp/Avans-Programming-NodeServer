d456b3f170e34561a1837eb93f3cc9eeee29fe7d
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-04-19 15:09:13 +0200

    Added HTTP request handler

---
+++ Server.js
@@ -0,0 +1,14 @@
+let http = require('http');
+
+let json = JSON.stringify({
+    'firstname': 'Marco',
+    'lastname': 'van Poortvliet'
+});
+
+http.createServer(function (request, response) {
+    console.log('=^.^= A kawaii neko appeared on your server =^.^=');
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(json);
+}).listen(3000);
+
+console.log('Server started listening on port 3000');
