d9df1c9ac5bb09ec2b35b08865bd9a3db7e8e2a8
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-04-19 16:07:30 +0200

    Added enviroment variable

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ http.createServer(function (request, response) {
     console.log('=^.^= A kawaii neko appeared on your server =^.^=');
     response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(json);
-}).listen(3000);
+}).listen(process.env.PORT || 3000);
 
 console.log('Server started listening on port 3000');
