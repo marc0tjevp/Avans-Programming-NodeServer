63673cd695d115e20cc59997e0f5849fe67d2465
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-04-19 21:25:41 +0200

    Added randomizer

--- server.js
+++ server.js
@@ -1,13 +1,72 @@
 let http = require('http');
 
-let json = JSON.stringify({
-    'firstname': 'Marco',
-    'lastname': 'van Poortvliet'
-});
-
 http.createServer(function (request, response) {
     console.log('=^.^= A kawaii neko appeared on your server =^.^=');
-    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+
+    var firstNameArray = [
+        'Lisa',
+        'Marco',
+        'Sander',
+        'Djim',
+        'Fer',
+        'Inge',
+        'Esther',
+        'Ruben',
+        'Eva',
+        'Jones',
+        'Chef',
+        'Floris',
+        'Rowenna',
+        'Kim',
+        'Kitty'
+    ];
+
+    var lastNameArray = [
+        'Tyem',
+        'van Poortvliet',
+        'Postma',
+        'Oomes',
+        'Schmidt',
+        'van Zetten',
+        'Zygmuntowitcz',
+        'Kolijn',
+        'Hoefman',
+        'de Poes',
+        'Konijn',
+        'Smits',
+        'Lequi',
+        'Schuurmans',
+        'Hoogendaale'
+    ]
+
+    var cityArray = [
+        'Tholen',
+        'Bergen op Zoom',
+        'Breda',
+        'Roosendaal',
+        'Heerle',
+        'Vlissingen',
+        'Rotterdam',
+        'Amsterdam',
+        'Marle',
+        'Maastricht'
+    ];
+
+    var randomFirstName = Math.floor(Math.random() * firstNameArray.length);
+    var randomLastName = Math.floor(Math.random() * lastNameArray.length);
+    var randomAge = Math.floor(Math.random() * 80 + 1);
+    var randomCity = Math.floor(Math.random() * cityArray.length);
+
+    let json = JSON.stringify({
+        'firstname': firstNameArray[randomFirstName],
+        'lastname': lastNameArray[randomLastName],
+        'age': randomAge,
+        'city': cityArray[randomCity]
+    });
+
     response.end(json);
 }).listen(process.env.PORT || 3000);
 
