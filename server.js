834e3986884ab06bddd40bc2af7fa84736355ef3
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-04-19 15:56:33 +0200

    Oops

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ http.createServer(function (request, response) {
     console.log('=^.^= A kawaii neko appeared on your server =^.^=');
     response.writeHead(200, {'Content-Type': 'application/json'});
     response.end(json);
-}).listen(80);
+}).listen(3000);
 
 console.log('Server started listening on port 3000');
